eda662a59cd22f9750dd80f904bbacec83bef664
fredkeemhaus <fredkeemhaus@gmail.com>
2023-03-08 17:15:17 +0900

    feat: change short cut

--- README.md
+++ README.md
@@ -1,71 +1,15 @@
-# naming-siba README
+# Naming-siba README
 
-This is the README for your extension "naming-siba". After writing up a brief description, we recommend including the following sections.
+# Korean
 
-## Features
+변수 이름을 짓기 위해 파파고와 구글을 더 이상 왔다갔다 하기 귀찮아서 만들어 보았습니다.
+intelliJ에만 있어서 vscode에도 한 번 만들어 봤습니다.
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### 마우스 조작
 
-For example if there is an image subfolder under your extension project workspace:
+텍스트를 드래그 한 뒤, 오른쪽 마우스를 클릭하면 노출됩니다.
 
-\!\[feature X\]\(images/feature-x.png\)
+### 단축키 조작
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+camelCase 단축키는 `alt` + `shift` + `a`
+snake_case 단축키는 `alt` + `shift` + `s` 입니다.
--- src/extension.ts
+++ src/extension.ts
@@ -1,65 +1,3 @@
-// import * as vscode from "vscode";
-// import axios from "axios";
-// import { camelCase } from "lodash";
-
-// export function activate(context: vscode.ExtensionContext) {
-//   let disposable = vscode.commands.registerCommand(
-//     "extension.translateToCamelCaseEnglish",
-//     () => {
-//       const editor = vscode.window.activeTextEditor;
-//       if (editor) {
-//         const selection = editor.selection;
-//         const text = editor.document.getText(selection);
-//         detectLanguageAndTranslate(text, editor, selection);
-//       }
-//     }
-//   );
-
-//   context.subscriptions.push(disposable);
-// }
-
-// async function detectLanguageAndTranslate(
-//   text: string,
-//   editor: vscode.TextEditor,
-//   selection: vscode.Selection
-// ) {
-//   const apiKey = "AIzaSyCcu4I4rBD_Xos7W-7iGX8EqzV8KmC19h0"; // 구글 번역 API 키 입력
-//   const detectUrl = `https://translation.googleapis.com/language/translate/v2/detect?key=${apiKey}`;
-//   const translateUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
-
-//   const detectData = {
-//     q: text,
-//   };
-
-//   try {
-//     const detectResponse = await axios.post(detectUrl, detectData);
-//     const detectedLanguage = detectResponse.data.data.detections[0][0].language;
-
-//     if (detectedLanguage === "en") {
-//       vscode.window.showInformationMessage(
-//         "Selected text is already in English"
-//       );
-//     } else {
-//       const translateData = {
-//         q: text,
-//         target: "en",
-//       };
-
-//       const translateResponse = await axios.post(translateUrl, translateData);
-//       const translatedText =
-//         translateResponse.data.data.translations[0].translatedText;
-//       const camelCaseText = camelCase(translatedText);
-//       editor.edit((editBuilder) => {
-//         editBuilder.replace(selection, camelCaseText);
-//       });
-//     }
-//   } catch (error: any) {
-//     vscode.window.showErrorMessage(
-//       `Failed to translate text: ${error.message}`
-//     );
-//   }
-// }
-
 import * as vscode from "vscode";
 import axios from "axios";
 import { camelCase, snakeCase } from "lodash";
@@ -99,9 +37,8 @@ async function detectLanguageAndTranslate(
   selection: vscode.Selection,
   transformFn: (text: string) => string
 ) {
-  const apiKey = "AIzaSyCcu4I4rBD_Xos7W-7iGX8EqzV8KmC19h0"; // 구글 번역 API 키 입력
-  const detectUrl = `https://translation.googleapis.com/language/translate/v2/detect?key=${apiKey}`;
-  const translateUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+  const detectUrl = `https://translation.googleapis.com/language/translate/v2/detect?key=${process.env.API_KEY}`;
+  const translateUrl = `https://translation.googleapis.com/language/translate/v2?key=${process.env.API_KEY}`;
 
   const detectData = {
     q: text,
